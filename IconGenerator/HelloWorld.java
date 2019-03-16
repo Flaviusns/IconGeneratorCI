c0589c996f45b2714c927564860edd9b1af3d5e8
Flavius Stan <flaviusns@outlook.com>
2019-03-16 10:45:22 +0100

    Initial aproach to resize Image. Create resXxXxY function

---
+++ IconGenerator/HelloWorld.java
@@ -0,0 +1,29 @@
+package IconGenerator;
+
+import IconGenerator.ImageRescaling;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        ImageRescaling im = new ImageRescaling();
+        File pathToFile = new File("/Users/flaviusstan/Downloads/IMG_9379.jpeg");
+        try {
+            Image image = ImageIO.read(pathToFile);
+            BufferedImage resizedImage = im.resXxXxY(image, 5, 1);
+            System.out.println(Integer.toString(resizedImage.getWidth()));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
